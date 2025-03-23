8808e13682a4de025bf48a4704e423cbd9540214
Bogdan <bjankovic16@gmail.com>
2025-03-23 17:24:36 +0100

    removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,10 @@ import CaseDetail from './pages/CaseDetail';
 import './index.css' // or wherever your Tailwind file is
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/cases/:id" element={<CaseDetail />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/cases/:id" element={<CaseDetail />} />
+    </Routes>
+  </BrowserRouter>
 );
